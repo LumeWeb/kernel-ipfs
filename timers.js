f69ff102cc5384ce1eb574f602bf053282a13228
Derrick Hammer <derrick@derrickhammer.com>
2023-04-09 02:38:32 -0400

    Revert "Revert "* Remove timers.js from build.js inject.""
    This reverts commit 184eed8be891106622b9e79644df817116e451a6.

--- timers.js
+++
@@ -1,8 +0,0 @@
-import { setTimeout, setInterval, clearTimeout, clearInterval } from 'timers-browserify'
-
-var scope = typeof self !== "undefined" && self || typeof self !== "undefined" && self || window;
-
-scope.setTimeout = setTimeout;
-scope.setInterval = setInterval;
-scope.clearTimeout = clearTimeout;
-scope.clearInterval = clearInterval;
